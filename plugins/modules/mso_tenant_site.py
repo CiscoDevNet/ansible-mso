d7ffac8fb10d0f6a62fd1b798eb8c4c03028f373
ssrish <ssrish@cisco.com>
2020-06-26 17:43:23 -0700

    Changes made to module after review

--- plugins/modules/mso_tenant_site.py
+++ plugins/modules/mso_tenant_site.py
@@ -15,90 +15,85 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 module: mso_tenant_site
-short_description: Manage tenants with cloud sites
+short_description: Manage tenants with cloud sites.
 description:
 - Manage tenants with cloud sites on Cisco ACI Multi-Site.
 author:
 - Shreyas Srish (@shrsr)
-version_added: '2.8'
 options:
   tenant:
     description:
-    - The name of the tenant
+    - The name of the tenant.
     type: str
     required: yes
-    aliases: [ name ]
   site:
     description:
-    - The name of the site
-    - This can either be cloud site or non-cloud site
+    - The name of the site.
+    - This can either be cloud site or non-cloud site.
     type: str
     aliases: [ name ]
   cloud_account:
     description:
+    - Required for cloud site.
     - Account id of AWS in the form '000000000000'.
-    - Account id of Azure in the form 'uni/tn-(tenant_name)/act-[(subscriptionId)]-azure_vendor-azure'.
+    - Account id of Azure in the form 'uni/tn-(tenant_name)/act-[(subscription_id)]-azure_vendor-azure'.
+    - Example values inside account id of Azure '(tenant_name)=tenant_test and (subscription_id)=10'.
     type: str
   security_domains:
     description:
-    - List of security domains for cloud sites
+    - List of security domains for cloud sites.
     type: list
     default: []
+  aws_account_org:
+    description:
+    - AWS account for organization.
+    default: false
+    type: bool
   aws_trusted:
     description:
-    - AWS account's access in trusted mode. Set to true if no credentials are required.
+    - AWS account's access in trusted mode. Credentials are required, when set to false.
     type: bool
+  aws_access_key:
+    description:
+    - AWS account's access key id. This is required when aws_trusted is set to false.
+    type: str
   azure_access_type:
     description:
-    - Managed mode for Azure
-    - Credentials mode for Azure
-    - Shared mode if the attribute is not specified
-    choices: [ managed, credentials ]
+    - Managed mode for Azure.
+    - Unmanaged mode for Azure.
+    - Shared mode if the attribute is not specified.
+    choices: [ managed, unmanaged, shared ]
+    default: shared
     type: str
   azure_active_directory_id:
     description:
-    - Azure account's active directory id
-    - This attribute is required when azure_access_type is in credentials mode
+    - Azure account's active directory id.
+    - This attribute is required when azure_access_type is in unmanaged mode.
     type: str
-  aws_access_key:
-    description:
-    - AWS account's access key id
-    type: str
-  aws_account_org:
-    description:
-    - AWS account for organization
-    type: bool
   azure_active_directory_name:
     description:
-    - Azure account's active directory name
-    - This attribute is required when azure_access_type is in credentials mode
+    - Azure account's active directory name. Example being 'CiscoINSBUAd' as active directory name.
+    - This attribute is required when azure_access_type is in unmanaged mode.
     type: str
   azure_subscription_id:
     description:
     - Azure account's subscription id.
-    - This attribute is required when azure_access_type is in manged mode
-    - This attribute is required when azure_access_type is in credentials mode
+    - This attribute is required when azure_access_type is either in managed mode or unmanaged mode.
     type: str
   azure_application_id:
     description:
-    - Azure account's application id
-    - This attribute is required when azure_access_type is in manged mode
-    - This attribute is required when azure_access_type is in credentials mode
+    - Azure account's application id.
+    - This attribute is required when azure_access_type is either in managed mode or unmanaged mode.
     type: str
   azure_credential_name:
     description:
-    - Azure account's credential name
-    - This attribute is required when azure_access_type is in credentials mode
+    - Azure account's credential name.
+    - This attribute is required when azure_access_type is in unmanaged mode.
     type: str
   secret_key:
     description:
-    - secret key of AWS for untrusted account. Required in untrusted mode of AWS account.
-    - secret key of Azure for unmanaged identity. Required in unmanged mode of Azure account.
-    type: str
-  azure_vendor:
-    description:
-    - This is an azure attribute
-    default: azure
+    - secret key of AWS for untrusted account. Required when aws_trusted is set to false.
+    - secret key of Azure account for unmanaged identity. Required in unmanaged mode of Azure account.
     type: str
   state:
     description:
@@ -116,81 +111,96 @@ EXAMPLES = r'''
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
-    site: ansible_test
+    tenant: tenant_name
+    site: site_name
     state: present
   delegate_to: localhost
 
-- name: Associate aws site with a tenant
+- name: Associate AWS site with a tenant, with aws_trusted set to true
   cisco.mso.mso_tenant_site:
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
-    site: aws
+    tenant: tenant_name
+    site: site_name
+    cloud_account: '000000000000'
+    aws_trusted: true
+    state: present
+  delegate_to: localhost
+
+- name: Associate AWS site with a tenant, with aws_trusted set to false
+  cisco.mso.mso_tenant_site:
+    host: mso_host
+    username: admin
+    password: SomeSecretPassword
+    tenant: tenant_name
+    site: AWS
     cloud_account: '000000000000'
     aws_trusted: false
-    aws_access_key: 1
-    secret_key: 0
+    aws_access_key: '1'
+    secret_key: '0'
     aws_account_org: false
     state: present
   delegate_to: localhost
 
-- name: Associate azure site in credentials mode
+- name: Associate Azure site in managed mode
   mso.cisco.mso_tenant_site:
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
-    site: azure
-    azure_subscription_id: 9
-    azure_application_id: 100
-    azure_credential_name: cApicApp
-    secret_key: iins
-    azure_active_directory_id: 32
-    azure_active_directory_name: CiscoINSBUAd
-    azure_vendor: azure
+    tenant: tenant_name
+    site: site_name
     cloud_account: uni/tn-ansible_test/act-[9]-azure_vendor-azure
-    azure_access_type: credentials
+    azure_access_type: managed
+    azure_subscription_id: '9'
+    azure_application_id: '100'
     state: present
   delegate_to: localhost
 
-- name: Dissociate aws site
+- name: Associate Azure site in unmanaged mode
+  mso.cisco.mso_tenant_site:
+    host: mso_host
+    username: admin
+    password: SomeSecretPassword
+    tenant: tenant_name
+    site: site_name
+    cloud_account: uni/tn-ansible_test/act-[9]-azure_vendor-azure
+    azure_access_type: unmanaged
+    azure_subscription_id: '9'
+    azure_application_id: '100'
+    azure_credential_name: cApicApp
+    secret_key: iins
+    azure_active_directory_id: '32'
+    azure_active_directory_name: CiscoINSBUAd
+    state: present
+  delegate_to: localhost
+
+- name: Dissociate a site
   cisco.mso.mso_tenant_site:
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
-    site: aws
+    tenant: tenant_name
+    site: site_name
     state: absent
   delegate_to: localhost
 
-- name: Dissociate azure site
+- name: Query a site
   cisco.mso.mso_tenant_site:
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
-    site: azure
-    state: absent
-  delegate_to: localhost
-
-- name: Query aws tenant
-  cisco.mso.mso_tenant_site:
-    host: mso_host
-    username: admin
-    password: SomeSecretPassword
-    tenant: ansible_test
-    site: aws
+    tenant: tenant_name
+    site: site_name
     state: query
   delegate_to: localhost
 
-- name: Query all sites
+- name: Query all sites of a tenant
   cisco.mso.mso_tenant_site:
     host: mso_host
     username: admin
     password: SomeSecretPassword
-    tenant: ansible_test
+    tenant: tenant_name
     state: query
   delegate_to: localhost
   register: query_result
@@ -209,13 +219,12 @@ def main():
         tenant=dict(type='str', aliases=['name'], required=True),
         site=dict(type='str', aliases=['name']),
         cloud_account=dict(type='str'),
-        azure_vendor=dict(type='str', default='azure'),
         security_domains=dict(type='list', default=[]),
         aws_trusted=dict(type='bool'),
-        azure_access_type=dict(type='str', choices=['managed', 'credentials']),
+        azure_access_type=dict(type='str', default='shared', choices=['managed', 'unmanaged', 'shared']),
         azure_active_directory_id=dict(type='str'),
         aws_access_key=dict(type='str'),
-        aws_account_org=dict(type='bool'),
+        aws_account_org=dict(type='bool', default='false'),
         azure_active_directory_name=dict(type='str'),
         azure_subscription_id=dict(type='str'),
         azure_application_id=dict(type='str'),
@@ -235,7 +244,6 @@ def main():
 
     state = module.params.get('state')
     security_domains = module.params.get('security_domains')
-    azure_vendor = module.params.get('azure_vendor')
     cloud_account = module.params.get('cloud_account')
     azure_access_type = module.params.get('azure_access_type')
     azure_credential_name = module.params.get('azure_credential_name')
@@ -268,51 +276,62 @@ def main():
         cloudAccount=cloud_account,
     )
 
-    if not cloud_account:
-        payload = payload
-
-    if cloud_account is not None:
+    if cloud_account:
         if 'azure' in cloud_account:
-            if azure_access_type is None:
-                payload = payload
+            azure_account = dict(
+                accessType=azure_access_type,
+                securityDomains=security_domains,
+                vendor='azure',
+            )
 
-            if azure_access_type is not None:
-                azure_account = dict(
-                    accessType=azure_access_type,
-                    securityDomains=security_domains,
-                    vendor=azure_vendor,
+            payload['azureAccount'] = [azure_account]
+
+            cloudSubscription = dict(
+                cloudSubscriptionId=azure_subscription_id,
+                cloudApplicationId=azure_application_id,
+            )
+
+            payload['azureAccount'][0]['cloudSubscription'] = cloudSubscription
+
+            if azure_access_type == 'shared':
+                payload['azureAccount'] = []
+
+            if azure_access_type == 'managed':
+                if not azure_subscription_id:
+                    mso.fail_json(msg="azure_susbscription_id is required when in managed mode.")
+                if not azure_application_id:
+                    mso.fail_json(msg="azure_application_id is required when in managed mode.")
+                payload['azureAccount'][0]['cloudApplication'] = []
+                payload['azureAccount'][0]['cloudActiveDirectory'] = []
+
+            if azure_access_type == 'unmanaged':
+                if not azure_subscription_id:
+                    mso.fail_json(msg="azure_subscription_id is required when in unmanaged mode.")
+                if not azure_application_id:
+                    mso.fail_json(msg="azure_application_id is required when in unmanaged mode.")
+                if not secret_key:
+                    mso.fail_json(msg="secret_key is required when in unmanaged mode.")
+                if not azure_active_directory_id:
+                    mso.fail_json(msg="azure_active_directory_id is required when in unmanaged mode.")
+                if not azure_active_directory_name:
+                    mso.fail_json(msg="azure_active_directory_name is required when in unmanaged mode.")
+                if not azure_credential_name:
+                    mso.fail_json(msg="azure_credential_name is required when in unmanaged mode.")
+                azure_account.update(
+                    accessType='credentials',
                 )
-
-                payload['azureAccount'] = [azure_account]
-                cloudSubscription = dict(cloudSubscriptionId=azure_subscription_id, cloudApplicationId=azure_application_id)
-                payload['azureAccount'][0]['cloudSubscription'] = cloudSubscription
-                cloudApplication = dict(cloudApplicationId=azure_application_id, cloudCredentialName=azure_credential_name,
-                                        secretKey=secret_key, cloudActiveDirectoryId=azure_active_directory_id)
-                cloudActiveDirectory = dict(cloudActiveDirectoryId=azure_active_directory_id, cloudActiveDirectoryName=azure_active_directory_name)
-
-                if azure_access_type == 'managed':
-                    if not azure_subscription_id:
-                        mso.fail_json(msg="azure_susbscription_id is required when in managed mode.")
-                    if not azure_application_id:
-                        mso.fail_json(msg="azure_application_id is required when in managed mode.")
-                    payload['azureAccount'][0]['cloudApplication'] = []
-                    payload['azureAccount'][0]['cloudActiveDirectory'] = []
-
-                if azure_access_type == 'credentials':
-                    if not azure_subscription_id:
-                        mso.fail_json(msg="azure_subscription_id is required when in credentials mode.")
-                    if not azure_application_id:
-                        mso.fail_json(msg="azure_application_id is required when in credentials mode.")
-                    if not secret_key:
-                        mso.fail_json(msg="secret_key is required when in credentials mode.")
-                    if not azure_active_directory_id:
-                        mso.fail_json(msg="azure_active_directory_id is required when in credentials mode.")
-                    if not azure_active_directory_name:
-                        mso.fail_json(msg="azure_active_directory_name is required when in credentials mode.")
-                    if not azure_credential_name:
-                        mso.fail_json(msg="azure_credential_name is required when in credentials mode.")
-                    payload['azureAccount'][0]['cloudApplication'] = [cloudApplication]
-                    payload['azureAccount'][0]['cloudActiveDirectory'] = [cloudActiveDirectory]
+                cloudApplication = dict(
+                    cloudApplicationId=azure_application_id,
+                    cloudCredentialName=azure_credential_name,
+                    secretKey=secret_key,
+                    cloudActiveDirectoryId=azure_active_directory_id
+                )
+                cloudActiveDirectory = dict(
+                    cloudActiveDirectoryId=azure_active_directory_id,
+                    cloudActiveDirectoryName=azure_active_directory_name
+                )
+                payload['azureAccount'][0]['cloudApplication'] = [cloudApplication]
+                payload['azureAccount'][0]['cloudActiveDirectory'] = [cloudActiveDirectory]
 
         else:
             aws_account = dict(
@@ -322,6 +341,7 @@ def main():
                 secretKey=secret_key,
                 isAccountInOrg=aws_account_org,
             )
+
             if not aws_trusted:
                 if not aws_access_key:
                     mso.fail_json(msg="aws_access_key is a required field in untrusted mode.")
@@ -364,7 +384,7 @@ def main():
 
         mso.existing = mso.proposed
 
-    if not module.check_mode:
+    if not module.check_mode and mso.proposed != mso.previous:
         mso.request(tenant_path, method='PATCH', data=ops)
 
     mso.exit_json()

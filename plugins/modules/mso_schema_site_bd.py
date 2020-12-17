e50eda08519d18cc108f4a75bccc4eedcbecc140
cindy zhao <xinyuezhao96@gmail.com>
2020-12-16 23:37:11 -0800

    Remove space in  template name

--- plugins/modules/mso_schema_site_bd.py
+++ plugins/modules/mso_schema_site_bd.py
@@ -140,7 +140,7 @@ def main():
 
     schema = module.params.get('schema')
     site = module.params.get('site')
-    template = module.params.get('template')
+    template = module.params.get('template').replace(' ', '')
     bd = module.params.get('bd')
     host_route = module.params.get('host_route')
     svi_mac = module.params.get('svi_mac')

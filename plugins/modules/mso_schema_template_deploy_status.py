32ca5fa6838aa5ce6e4bc0e2545f747f01b66e61
ssrish <ssrish@cisco.com>
2021-02-10 14:29:13 -0800

    Added site parameter for query

--- plugins/modules/mso_schema_template_deploy_status.py
+++ plugins/modules/mso_schema_template_deploy_status.py
@@ -14,9 +14,9 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 ---
 module: mso_schema_template_deploy_status
-short_description: Check status of objects before deployment to site
+short_description: Check query of objects before deployment to site
 description:
-- Check status of objects in a template of a schema
+- Check query of objects in a template of a schema
 author:
 - Shreyas Srish (@shrsr)
 options:
@@ -29,35 +29,62 @@ options:
     description:
     - The name of the template.
     type: str
+  site:
+    description:
+    - The name of the site.
+    type: str
   state:
     description:
-    - Use C(status) for listing status of objects.
+    - Use C(query) for listing query of objects.
     type: str
-    choices: [ status ]
-    default: status
+    choices: [ query ]
+    default: query
 extends_documentation_fragment: cisco.mso.modules
 '''
 
 EXAMPLES = r'''
 
-- name: Query status of objects in a template before deployment
+- name: Query status of objects in a template
   cisco.mso.mso_schema_template_deploy_status:
     host: mso_host
     username: admin
     password: SomeSecretPassword
     schema: Schema 1
-    template: Template1
-    state: status
+    template: Template 1
+    state: query
   delegate_to: localhost
   register: query_result
 
-- name: Query status of objects in all templates before deployment
+- name: Query status of objects using site
   cisco.mso.mso_schema_template_deploy_status:
     host: mso_host
     username: admin
     password: SomeSecretPassword
     schema: Schema 1
-    state: status
+    site: ansible_test
+    state: query
+  delegate_to: localhost
+  register: query_result
+
+- name: Query status of objects in a template asscociated with a site
+  cisco.mso.mso_schema_template_deploy_status:
+    host: mso_host
+    username: admin
+    password: SomeSecretPassword
+    schema: Schema 1
+    template: Template 1
+    site: ansible_test
+    state: query
+  delegate_to: localhost
+  register: query_result
+
+- name: Query status of objects in all templates
+  cisco.mso.mso_schema_template_deploy_status:
+    host: mso_host
+    username: admin
+    password: SomeSecretPassword
+    schema: Schema 1
+    state: query
   delegate_to: localhost
   register: query_result
 '''
@@ -74,14 +101,15 @@ def main():
     argument_spec.update(
         schema=dict(type='str', aliases=['name']),
         template=dict(type='str'),
-        state=dict(type='str', default='status', choices=['status']),
+        site=dict(type='str'),
+        state=dict(type='str', default='query', choices=['query']),
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
         supports_check_mode=True,
         required_if=[
-            ['state', 'status', ['schema']],
+            ['state', 'query', ['schema']],
         ],
     )
 
@@ -89,6 +117,7 @@ def main():
     template = module.params.get('template')
     if template is not None:
         template = template.replace(' ', '')
+    site = module.params.get('site')
     state = module.params.get('state')
 
     mso = MSOModule(module)
@@ -103,17 +132,30 @@ def main():
     else:
         mso.fail_json(msg="Schema '{0}' not found.".format(schema))
 
-    if state == 'status':
-        mso.existing = mso.request(path, method='GET')
+    if state == 'query':
+        get_data = mso.request(path, method='GET')
+        mso.existing = []
         if template:
-            for objects in mso.existing.get('policyStates'):
-                if objects.get('templateName') == template:
-                    mso.existing = objects
-                    break
-                else:
-                    mso.existing = {}
+            for configuration_objects in get_data.get('policyStates'):
+                if configuration_objects.get('templateName') == template:
+                    mso.existing.append(configuration_objects)
             if not mso.existing:
                 mso.fail_json(msg="Template '{0}' not found.".format(template))
+
+        if site:
+            mso.existing.clear()
+            for configuration_objects in get_data.get('policyStates'):
+                if configuration_objects.get('siteId') == mso.lookup_site(site):
+                    if template:
+                        if configuration_objects.get('templateName') == template:
+                            mso.existing = configuration_objects
+                    else:
+                        mso.existing.append(configuration_objects)
+            if template is not None and not mso.existing:
+                mso.fail_json(msg="Provided Template '{0}' not associated with Site '{1}'.".format(template, site))
+
+        if template is None and site is None:
+            mso.existing = get_data
 
     mso.exit_json()
 

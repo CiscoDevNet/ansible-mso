4d6ebfe369d7f10f2fcf59bef271f911eb1be438
ssrish <ssrish@cisco.com>
2021-05-19 11:37:00 -0700

    add option is_bd_subnet and when conditions to test cases

--- plugins/modules/mso_schema_template_anp_epg.py
+++ plugins/modules/mso_schema_template_anp_epg.py
@@ -345,7 +345,7 @@ def main():
     elif state == 'present':
         bd_ref = mso.make_reference(bd, 'bd', schema_id, template)
         vrf_ref = mso.make_reference(vrf, 'vrf', schema_id, template)
-        subnets = mso.make_subnets(subnets, querier=False)
+        subnets = mso.make_subnets(subnets, is_bd_subnet=False)
 
         if display_name is None and not mso.existing:
             display_name = epg

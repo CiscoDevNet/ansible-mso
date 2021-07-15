b91b9d9f39bad77a3410c19e42af6aa5425fbf5e
anvitha jain <anvjain@cisco.com>
2021-07-15 03:10:48 -0700

    Added private link level parameter to ANP EPG at site level

--- plugins/modules/mso_schema_site_anp_epg.py
+++ plugins/modules/mso_schema_site_anp_epg.py
@@ -45,6 +45,11 @@ options:
     - The name of the EPG to manage.
     type: str
     aliases: [ name ]
+  private_link_label:
+    description:
+    - The private link label used to represent this subnet.
+    - This parameter is available for MSO version greater than 3.3.
+    type: str
   state:
     description:
     - Use C(present) or C(absent) for adding or removing.
@@ -129,6 +134,7 @@ def main():
         template=dict(type='str', required=True),
         anp=dict(type='str', required=True),
         epg=dict(type='str', aliases=['name']),  # This parameter is not required for querying all objects
+        private_link_label=dict(type='str'),
         state=dict(type='str', default='present', choices=['absent', 'present', 'query']),
     )
 
@@ -146,6 +152,7 @@ def main():
     template = module.params.get('template').replace(' ', '')
     anp = module.params.get('anp')
     epg = module.params.get('epg')
+    private_link_label = module.params.get('private_link_label')
     state = module.params.get('state')
 
     mso = MSOModule(module)
@@ -153,45 +160,109 @@ def main():
     # Get schema objects
     schema_id, schema_path, schema_obj = mso.query_schema(schema)
 
+        # Get template
+    templates = [t.get('name') for t in schema_obj.get('templates')]
+    if template not in templates:
+        mso.fail_json(msg="Provided template '{0}' does not exist. Existing templates: {1}".format(template, ', '.join(templates)))
+    template_idx = templates.index(template)
+
     # Get site
     site_id = mso.lookup_site(site)
+
+    # Get site_idx
     if 'sites' not in schema_obj:
         mso.fail_json(msg="No site associated with template '{0}'. Associate the site with the template using mso_schema_site.".format(template))
     sites = [(s.get('siteId'), s.get('templateName')) for s in schema_obj.get('sites')]
     if (site_id, template) not in sites:
-        mso.fail_json(msg="Provided site/template '{0}-{1}' does not exist. Existing sites/templates: {2}".format(site, template, ', '.join(sites)))
+        mso.fail_json(msg="Provided site-template association '{0}-{1}' does not exist.".format(site, template))
 
     # Schema-access uses indexes
     site_idx = sites.index((site_id, template))
     # Path-based access uses site_id-template
     site_template = '{0}-{1}'.format(site_id, template)
 
+    payload = {}
+    ops = []
+    op_path = ''
+
     # Get ANP
     anp_ref = mso.anp_ref(schema_id=schema_id, template=template, anp=anp)
-    anps = [a.get('anpRef') for a in schema_obj.get('sites')[site_idx]['anps']]
-    if anp_ref not in anps:
-        mso.fail_json(msg="Provided anp '{0}' does not exist. Existing anps: {1}".format(anp, ', '.join(anps)))
-    anp_idx = anps.index(anp_ref)
+    anps = [a.get('anpRef') for a in schema_obj['sites'][site_idx]['anps']]
+    anps_in_temp = [a.get('name') for a in schema_obj['templates'][template_idx]['anps']]
+    if anp not in anps_in_temp:
+        mso.fail_json(msg="Provided anp '{0}' does not exist. Existing anps: {1}".format(anp, ', '.join(anps_in_temp)))
+    else:
+        # Get anp index at template level
+        template_anp_idx = anps_in_temp.index(anp)
 
-    # Get EPG
-    epg_ref = mso.epg_ref(schema_id=schema_id, template=template, anp=anp, epg=epg)
-    epgs = [e.get('epgRef') for e in schema_obj.get('sites')[site_idx]['anps'][anp_idx]['epgs']]
-    if epg is not None and epg_ref in epgs:
-        epg_idx = epgs.index(epg_ref)
-        epg_path = '/sites/{0}/anps/{1}/epgs/{2}'.format(site_template, anp, epg)
-        mso.existing = schema_obj.get('sites')[site_idx]['anps'][anp_idx]['epgs'][epg_idx]
+    # If anp not at site level but exists at template level
+    if anp_ref not in anps:
+        op_path = '/sites/{0}/anps'.format(site_template)
+        payload = dict(
+            anpRef=dict(
+                schemaId=schema_id,
+                templateName=template,
+                anpName=anp,
+            ),
+        )
+    else:
+        # Get anp index at site level
+        anp_idx = anps.index(anp_ref)
+
+    if epg is not None:
+        # Get EPG
+        epg_ref = mso.epg_ref(schema_id=schema_id, template=template, anp=anp, epg=epg)
+        new_epg = dict(
+            epgRef=dict(
+                schemaId=schema_id,
+                templateName=template,
+                anpName=anp,
+                epgName=epg,
+            )
+        )
+
+        # If anp exists at site level
+        if 'anpRef' not in payload:
+            epgs = [e.get('epgRef') for e in schema_obj['sites'][site_idx]['anps'][anp_idx]['epgs']]
+
+        # If anp already at site level AND if epg not at site level (or) anp not at site level?
+        if ('anpRef' not in payload and epg_ref not in epgs) or 'anpRef' in payload:
+            epgs_in_temp = [e.get('name') for e in schema_obj['templates'][template_idx]['anps'][template_anp_idx]['epgs']]
+
+            # If EPG not at template level - Fail
+            if epg not in epgs_in_temp:
+                mso.fail_json(msg="Provided EPG '{0}' does not exist. Existing EPGs: {1}".format(epg, ', '.join(epgs_in_temp)))
+
+            # EPG at template level but not at site level. Create payload at site level for EPG
+            else:
+                # If anp not in payload then, anp already exists at site level. New payload will only have new EPG payload
+                if 'anpRef' not in payload:
+                    op_path = '/sites/{0}/anps/{1}/epgs'.format(site_template, anp)
+                    payload = new_epg
+                else:
+                    # If anp in payload, anp exists at site level. Update payload with EPG payload
+                    payload['epgs'] = [new_epg]
+
+        # Get index of EPG at site level
+        else:
+            epg_idx = epgs.index(epg_ref)
+            epg_path = '/sites/{0}/anps/{1}/epgs/{2}'.format(site_template, anp, epg)
+            mso.existing = schema_obj.get('sites')[site_idx]['anps'][anp_idx]['epgs'][epg_idx]
+            payload = new_epg
+
+    ops = []
 
     if state == 'query':
+        if anp_ref not in anps:
+            mso.fail_json(msg="Provided anp '{0}' does not exist at site level.".format(anp))
         if epg is None:
             mso.existing = schema_obj.get('sites')[site_idx]['anps'][anp_idx]['epgs']
         elif not mso.existing:
             mso.fail_json(msg="EPG '{epg}' not found".format(epg=epg))
         mso.exit_json()
 
-    epgs_path = '/sites/{0}/anps/{1}/epgs'.format(site_template, anp)
-    ops = []
-
     mso.previous = mso.existing
+
     if state == 'absent':
         if mso.existing:
             mso.sent = mso.existing = {}
@@ -199,19 +270,15 @@ def main():
 
     elif state == 'present':
 
-        payload = dict(
-            epgRef=dict(
-                schemaId=schema_id,
-                templateName=template,
-                anpName=anp,
-                epgName=epg,
-            ),
-        )
+        if private_link_label is not None:
+          payload['privateLinkLabel'] = dict(name=private_link_label)
 
         mso.sanitize(payload, collate=True)
 
-        if not mso.existing:
-            ops.append(dict(op='add', path=epgs_path + '/-', value=mso.sent))
+        if mso.existing:
+            ops.append(dict(op='replace', path=epg_path, value=mso.sent))
+        else:
+            ops.append(dict(op='add', path=op_path + '/-', value=mso.sent))
 
         mso.existing = mso.proposed
 

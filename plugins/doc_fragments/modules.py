ebacd71ce99a1048e70ac44a00f707b15a1fd778
Lionel Hercot <lhercot@cisco.com>
2021-12-09 12:56:49 +0100

    Fix default username when using HTTP API connection plugin

--- plugins/doc_fragments/modules.py
+++ plugins/doc_fragments/modules.py
@@ -28,7 +28,6 @@ options:
     - The username to use for authentication.
     - If the value is not specified in the task, the value of environment variables C(MSO_USERNAME) or C(ANSIBLE_NET_USERNAME) will be used instead.
     type: str
-    default: admin
   password:
     description:
     - The password to use for authentication.

0a6f647a256897a5dc13bb1c00b1e0b6368ef0c1
Anvitha-Jain <anvjain@cisco.com>
2023-03-08 17:47:51 -0800

    [minor_change] Add route_reachability attribute to mso_schema_site_external_epg module (#335)

--- plugins/module_utils/schema.py
+++ plugins/module_utils/schema.py
@@ -112,6 +112,21 @@ class MSOSchema:
             self.mso.fail_json(msg=msg)
         self.schema_objects["template_anp_epg"] = match
 
+    def set_template_external_epg(self, external_epg, fail_module=True):
+        """
+        Get template external epg item that matches the name of an anp.
+        :param anp: Name of the anp to match. -> Str
+        :param fail_module: When match is not found fail the ansible module. -> Bool
+        :return: Template anp item. -> Item(Int, Dict) | None
+        """
+        self.validate_schema_objects_present(["template"])
+        kv_list = [KVPair("name", external_epg)]
+        match, existing = self.get_object_from_list(self.schema_objects["template"].details.get("externalEpgs"), kv_list)
+        if not match and fail_module:
+            msg = "Provided External EPG '{0}' not matching existing external_epg(s): {1}".format(external_epg, ", ".join(existing))
+            self.mso.fail_json(msg=msg)
+        self.schema_objects["template_external_epg"] = match
+
     def set_site(self, template_name, site_name, fail_module=True):
         """
         Get site item that matches the name of a site.

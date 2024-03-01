499c3c08ae08897aa213be5e63cd3e4e39812e22
akinross <akinross@cisco.com>
2024-03-01 09:56:15 +0100

    [ignore] Change function documentation comments in schema.py for set_template_anp_epg_contract

--- plugins/module_utils/schema.py
+++ plugins/module_utils/schema.py
@@ -114,8 +114,9 @@ class MSOSchema:
 
     def set_template_anp_epg_contract(self, contract_ref, relation_type, fail_module=True):
         """
-        Get template endpoint group item that matches the name of an epg.
-        :param epg: Name of the epg to match. -> Str
+        Get template endpoint group contract item that matches the reference and type of an contract.
+        :param contract_ref: Reference of the contract to match. -> Str
+        :param relation_type: Relation_type of the contract to match. -> Str
         :param fail_module: When match is not found fail the ansible module. -> Bool
         :return: Template epg item. -> Item(Int, Dict) | None
         """

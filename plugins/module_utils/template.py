f46db4d8f3adfd5eaa309dec350bf09c73f847b3
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2024-10-30 19:06:48 +0530

    [minor_change] Added ndo_l3out_interface_routing_policy to manage L3Out Interface Routing Policy

--- plugins/module_utils/template.py
+++ plugins/module_utils/template.py
@@ -269,3 +269,25 @@ class MSOTemplate:
         if uuid or name:  # Query a specific object
             return self.get_object_by_key_value_pairs("Node Settings", existing_objects, [KVPair("uuid", uuid) if uuid else KVPair("name", name)], fail_module)
         return existing_objects  # Query all objects
+
+    def get_l3out_interface_routing_policy_object(self, uuid=None, name=None, fail_module=False):
+        """
+        Get the L3Out Interface Routing Policy by UUID or Name.
+        :param uuid: UUID of the L3Out Interface Routing Policy to search for -> Str
+        :param name: Name of the L3Out Interface Routing Policy to search for -> Str
+        :param fail_module: When match is not found fail the ansible module -> Bool
+        :return: Dict | None | List[Dict] | List[]: The processed result which could be:
+                 When the UUID | Name is existing in the search list -> Dict
+                 When the UUID | Name is not existing in the search list -> None
+                 When both UUID and Name are None, and the search list is not empty -> List[Dict]
+                 When both UUID and Name are None, and the search list is empty -> List[]
+        """
+        existing_l3out_interface_routing_policy = self.template.get("tenantPolicyTemplate", {}).get("template", {}).get("l3OutIntfPolGroups", [])
+        if uuid or name:  # Query a specific object
+            return self.get_object_by_key_value_pairs(
+                "L3Out Interface Routing Policy",
+                existing_l3out_interface_routing_policy,
+                [KVPair("uuid", uuid) if uuid else KVPair("name", name)],
+                fail_module,
+            )
+        return existing_l3out_interface_routing_policy  # Query all objects

f1e41419f2fba08c6027ab0ca96abd00b202b98c
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2024-11-15 21:29:00 +0530

    [minor_change] Added append_update_ops_data to handle the latest ndo modules update operations

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -7,6 +7,8 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
+import copy
+
 
 def generate_api_endpoint(path, **kwargs):
     """
@@ -26,3 +28,52 @@ def generate_api_endpoint(path, **kwargs):
     # return full_url
 
     return path if not kwargs else "{0}?{1}".format(path, "&".join(["{0}={1}".format(key, value) for key, value in kwargs.items()]))
+
+
+def append_update_ops_data(ops, existing_data, update_path, value, *args, op="replace"):
+    """
+    Append Update ops payload data.
+    :param ops: Variable which contains the PATCH replace actions for the update operation -> List
+    :param existing_data: Variable which contains the existing data -> Dict
+    :param update_path: The object path is used to update an existing object -> Str
+    :param value: Input value of the attribute which needs to be updated -> Any
+    :param args: Extra arguments which is used to compare and update the existing data -> Any
+    :param op: Defaults to "replace" when not specified, value "remove" is used clear the existing configuration -> Str
+    :return: None
+                If args is not empty then the ops and existing_data are updated with the input value.
+    """
+
+    def recursive_update(data, current_path, keys, new_value):
+        if len(keys) == 0:
+            return
+        key = keys[0]
+
+        if len(keys) == 1:
+            # Update the existing configuration
+            if new_value is not None and data.get(key) != new_value:
+                data[key] = new_value
+                ops.append(
+                    dict(
+                        op=op,
+                        path="{}/{}".format(current_path, key),
+                        value=copy.deepcopy(new_value),
+                    )
+                )
+            # Clear the existing configuration
+            elif op == "remove":
+                data.pop(key, None)
+                ops.append(
+                    dict(
+                        op=op,
+                        path="{}/{}".format(current_path, key),
+                    )
+                )
+
+        elif key in data:
+            recursive_update(data[key], "{}/{}".format(current_path, key), keys[1:], new_value)
+
+    if args:
+        if isinstance(args[0], tuple):
+            recursive_update(existing_data, update_path, args[0], value)
+        else:
+            recursive_update(existing_data, update_path, args, value)

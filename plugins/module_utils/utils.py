b064a70729d64f8fac29f9f92122a70a77d2261b
Gaspard Micol <gmicol@cisco.com>
2024-11-25 13:31:49 -0500

    [ignore] Change Documentation and attributes name. Add function to convert pyhton list of dict into equivalent NDO API format.

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -146,3 +146,28 @@ def check_if_all_elements_are_none(values):
     :return: True if all elements are None, False otherwise. -> boo
     """
     return all(value is None for value in values)
+
+
+def format_list_dict(list_dict, keys_map):
+    """
+    Convert an Python list of dictionaries into its equivalent NDO API format.
+
+    :param list_dict: The Python list of dictionaries to format. -> List[Dict]
+    :param keys_map: the mapping from the Ansible argument's keys to NDO API keys. Can also include the map between values -> Dict
+    :return: The formated dictionary. -> Dict
+    """
+    if list_dict:
+
+        def format_dict(d):
+            formated_dict = {}
+            if d:
+                for key, value in keys_map.items():
+                    # keys_map can have values of type list including the API key string and the map conversion for values\
+                    if isinstance(value, list):
+                        formated_dict[value[0]] = value[1].get(list_dict.get(key))
+                    else:
+                        formated_dict[value] = list_dict.get(key)
+            return formated_dict
+
+        formated_list = [format_dict(d) for d in list_dict]
+        return formated_list

c818c972da9679a165b393ec10ec0cf5701938ac
samitab <samitab@cisco.com>
2025-04-05 15:20:11 +1000

    [minor_change] Added new module for Tenant SPAN Session.

--- plugins/module_utils/schema.py
+++ plugins/module_utils/schema.py
@@ -152,18 +152,19 @@ class MSOSchema:
             self.mso.fail_json(msg=msg)
         self.schema_objects["template_bd_dhcp_relay_policy"] = match
 
-    def set_template_anp(self, anp, fail_module=True):
+    def set_template_anp(self, anp, anp_uuid=None, fail_module=True):
         """
         Get template application profile item that matches the name of an anp.
         :param anp: Name of the anp to match. -> Str
+        :param anp: UUID of the anp to match. -> Str
         :param fail_module: When match is not found fail the ansible module. -> Bool
         :return: Template anp item. -> Item(Int, Dict) | None
         """
         self.validate_schema_objects_present(["template"])
-        kv_list = [KVPair("name", anp)]
+        kv_list = [KVPair("uuid", anp_uuid) if anp_uuid else KVPair("name", anp)]
         match, existing = self.get_object_from_list(self.schema_objects["template"].details.get("anps"), kv_list)
         if not match and fail_module:
-            msg = "Provided ANP '{0}' not matching existing anp(s): {1}".format(anp, ", ".join(existing))
+            msg = "Provided ANP '{0}' not matching existing anp(s): {1}".format(anp_uuid if anp_uuid else anp, ", ".join(existing))
             self.mso.fail_json(msg=msg)
         self.schema_objects["template_anp"] = match
 

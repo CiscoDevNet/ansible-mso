0a4dc59c297e0fff671aef3f9efd1ee6cfdfd630
Sabari Jaganathan <93724860+sajagana@users.noreply.github.com>
2025-06-12 20:43:13 +0530

    [minor_change] Added schema template contract service chain module

--- plugins/module_utils/constants.py
+++ plugins/module_utils/constants.py
@@ -244,3 +244,5 @@ QOS_CONGESTION_ALGORITHM_MAP = {"tail_drop": "tailDrop", "wred": "wred"}
 QOS_SCHEDULING_ALGORITHM_MAP = {"strict_priority": "strictPriority", "weighted_round_robin": "wrr"}
 QOS_PFC_SCOPE_MAP = {"fabric_wide": "fabricWide", "intra_tor": "intraTor"}
 COS_VALUES = ["cos0", "cos1", "cos2", "cos3", "cos4", "cos5", "cos6", "cos7", "unspecified"]
+
+CONTRACT_SERVICE_CHAIN_NODE_FILTER_MAP = {"allow_all": "allow-all", "filters_from_contract": "filters-from-contract"}

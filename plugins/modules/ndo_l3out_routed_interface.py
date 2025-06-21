6e0478c8a865c5819479af078343116fa94a5615
akinross <akinross@cisco.com>
2025-06-21 10:10:36 +0200

    [ignore] add module depenency for portchannel

--- plugins/modules/ndo_l3out_routed_interface.py
+++ plugins/modules/ndo_l3out_routed_interface.py
@@ -200,9 +200,12 @@ notes:
   The M(cisco.mso.ndo_template) module can be used for this.
 - The O(l3out) or O(l3out_uuid) must exist before using this module in your playbook.
   The M(cisco.mso.ndo_l3out_template) module can be used for this.
+- The O(port_channel) must exist before using this module in your playbook.
+  The M(cisco.mso.ndo_port_channel_interface) module can be used for this.
 seealso:
 - module: cisco.mso.ndo_template
 - module: cisco.mso.ndo_l3out_template
+- module: cisco.mso.ndo_port_channel_interface
 extends_documentation_fragment: cisco.mso.modules
 """
 

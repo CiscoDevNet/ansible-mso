336a28ed0a2d8af862bb92cb6b794b9297670cdf
akinross <akinross@cisco.com>
2025-07-07 21:50:25 +0200

    [ignore] add functionality to template to fail when match on name is not found

--- plugins/module_utils/templates.py
+++ plugins/module_utils/templates.py
@@ -13,14 +13,14 @@ class MSOTemplates:
         self.templates_by_id = {}
         self.templates_by_name = {}
 
-    def get_template(self, template_type, template_name, template_id, refresh=False):
+    def get_template(self, template_type, template_name, template_id, refresh=False, fail_module=False):
         if not refresh:
             if template_id in self.templates_by_id:
                 return self.templates_by_id[template_id]
             elif (template_name, TEMPLATE_TYPES[template_type]["template_type"]) in self.templates_by_name:
                 return self.templates_by_name[(template_name, TEMPLATE_TYPES[template_type]["template_type"])]
 
-        new_template = MSOTemplate(self.mso, template_type, template_name, template_id)
+        new_template = MSOTemplate(self.mso, template_type, template_name, template_id, fail_module=fail_module)
         self.templates_by_id[new_template.template_id] = new_template
         self.templates_by_name[(new_template.template_name, new_template.template_type)] = new_template
         return new_template

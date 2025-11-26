1b92f2099c72e5d1af6d8e1228d8b6e669e8647c
Gaspard Micol <gmicol@cisco.com>
2025-11-26 12:32:59 -0500

    [ignore] Add template_id and schema_id as new configurable attributes for application template object references.

--- plugins/doc_fragments/bridge_domain_references.py
+++ plugins/doc_fragments/bridge_domain_references.py
@@ -35,12 +35,22 @@ options:
             type: str
           schema:
             description:
-            - The schema associated with the Bridge Domain.
-            required: true
+            - The name of the schema associated with the Bridge Domain.
+            - This parameter can be used instead of O(bridge_domain.reference.schema_id).
+            type: str
+          schema_id:
+            description:
+            - The schema ID associated with the Bridge Domain.
+            - This parameter can be used instead of O(bridge_domain.reference.schema).
             type: str
           template:
             description:
+            - The name of the template associated with the Bridge Domain.
+            - This parameter can be used instead of O(bridge_domain.reference.template_id).
+            type: str
+          template_id:
+            description:
             - The template ID associated with the Bridge Domain.
-            required: true
+            - This parameter can be used instead of O(bridge_domain.reference.template).
             type: str
 """

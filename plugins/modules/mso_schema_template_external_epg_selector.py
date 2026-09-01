f7db9d69abf5bad4a891b7e0e14bf66975f26ce5
akinross <akinross@cisco.com>
2026-09-01 08:39:35 +0200

    [minor_change] deprecate mso_schema_template_external_epg_selector module

--- plugins/modules/mso_schema_template_external_epg_selector.py
+++ plugins/modules/mso_schema_template_external_epg_selector.py
@@ -70,6 +70,10 @@ options:
     type: str
     choices: [ absent, present, query ]
     default: present
+deprecated:
+  removed_in: '4.0.0'
+  why: This module manages cloud-specific features that are no longer supported in Nexus Dashboard 4.x (NDO 5.x) releases.
+  alternative: No alternative is available.
 seealso:
 - module: cisco.mso.mso_schema_template_external_epg
 extends_documentation_fragment: cisco.mso.modules

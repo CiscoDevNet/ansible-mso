09613def56540d65057cc65ff5d665e8bb4b761e
akinross <akinross@cisco.com>
2026-09-03 13:22:39 +0200

    [minor_change] deprecate mso_remote_location module

--- plugins/modules/mso_remote_location.py
+++ plugins/modules/mso_remote_location.py
@@ -79,6 +79,10 @@ options:
     type: str
     choices: [ absent, present, query ]
     default: present
+deprecated:
+  removed_in: '4.0.0'
+  why: This module is no longer functional on Nexus Dashboard (ND) 4.0+ / NDO 5.0+ releases.
+  alternative: No alternative is available.
 extends_documentation_fragment: cisco.mso.modules
 """
 
